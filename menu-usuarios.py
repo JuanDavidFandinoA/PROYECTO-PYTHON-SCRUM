04948a483612d3d028a2efb16dc8b8e79aa53154
LuisEderLeon <luiseder.leonvesga@gmail.com>
2024-06-27 16:49:22 -0500

    rework Json mejorados
    Co-authored-by: JoseDaniel C.M <JoseDanielCM@users.noreply.github.com>

--- menu-usuarios.py
+++ menu-usuarios.py
@@ -1,24 +1,33 @@
 from funcionesGlobales import *
+CAT_ACCION=1
+CAT_BIOGRAFIA=2
+CAT_FANTASIA=3
+CAT_FICCION=4
+CAT_ROMANCE=5
+
 
 def menu_usuarios():
-    print("Ingreso de usuario exitoso")
-    print("*******************************************************")
-    print("Presiona ( 1 ) para acceder al buscador de libros")
-    print("Presiona ( 2 ) para acceder a los generos disponibles")
-    print("Presiona ( 3 ) para comprar libros")
-    print("Presiona ( 0 ) para salir")
-    print("*******************************************************")
-    opcion = listaOpciones("Ingrese la opcion que desea escoger","Por favor ingrese una opcion valida",3)
-    print("*******************************************************")
-    
-    if opcion == 1:
-        buscador()
-    elif opcion==2:
-        mostrar_generos()
-    elif opcion==3:
-        comprar_libro()
-    else:
-        print("Salida exitosa :) \n")
+    while(True):
+        print("Ingreso de usuario exitoso")
+        print("\n*******************************************************\n")
+        print("Presiona ( 1 ) para acceder al buscador de libros")
+        print("Presiona ( 2 ) para acceder a los generos disponibles")
+        print("Presiona ( 3 ) para comprar libros")
+        print("Presiona ( 0 ) para salir")
+        print("\n*******************************************************\n")
+        opcion = listaOpciones("Ingrese la opcion que desea escoger","Por favor ingrese una opcion valida",3)
+        print("\n*******************************************************\n")
+        
+        if opcion == 1:
+            buscador()
+        elif opcion==2:
+            mostrar_generos()
+        elif opcion==3:
+            comprar_libro()
+        else:
+            print("Salida exitosa :) \n")
+            break
+        input("Oprima Enter para continuar\n")
 
 
 
@@ -26,43 +35,57 @@ def menu_usuarios():
 def buscador():
     nombre = input("Ingrese el nombre del libro que desea buscar: ")
     print("")
-    generos = ["ficcion"]
-    for genero in generos:
-        datos = leerJson(genero)
-        for libro in datos:
-            if libro.get("nombre") == nombre:
-                print("Nombre: " + libro.get("nombre"))
-                print("Autor(a): " + libro.get("autor"))
-                print("Edad: " + str(libro.get("edad")))
-                print("Descripción: " + libro.get("descripcion"))
-                return None
-    print("Libro no encontrado")
+
+    datos_libros=leerJson("libros")
+    print(datos_libros)
+
+    # generos = ["ficcion"]
+    # for genero in generos:
+    #     datos = leerJson(genero)
+    #     for libro in datos:
+    #         if libro.get("nombre") == nombre:
+    #             print("Nombre: " + libro.get("nombre"))
+    #             print("Autor(a): " + libro.get("autor"))
+    #             print("Edad: " + str(libro.get("edad")))
+    #             print("Descripción: " + libro.get("descripcion"))
+    #             return None
+    # print("Libro no encontrado")
 
 def mostrar_generos():
-    print("""
-Los generos disponibles son:
-0 - Biografia
-1 - Ficcion
-2 - Accion
+    print("""Los generos disponibles son:
+          
+1 - Accion
+2 - Biografia
 3 - Fantasia
-4 - Romance
+4 - Ficcion
+5 - Romance
+0 - Salir
+          
+*******************************************************
+""")
+    generos_disponibles = leerJson("categorias").keys()
+    genero_escogido = listaOpciones("Ingrese el numero del genero que desea buscar","Por favor ingrese un numero valido",5)
+    if genero_escogido == 0:
+        print("")
+        return None
+    else:
+        genero_escogido -= 1
+    genero_escogido = generos_disponibles[genero_escogido]
 
-*******************************************************""")
-    generos_disponibles=["biografía","ficcion","accion","fantasia","romance"]
-    genero_escogido = listaOpciones("Ingrese el numero del genero que desea buscar","Por favor ingrese un numero valido",4)
-    genero_escogido=generos_disponibles[genero_escogido]
+    datos_ficcion=leerJson("libros")
 
-    datos_ficcion=leerJson(genero_escogido)
-
+    print("\n-------------------------------------------------------\n")
     print(f"Los libros encontrados para el genero {genero_escogido} son: \n")
     for libro in datos_ficcion:
         print("Nombre: " + libro["nombre"])
         print("Autor(a): " + libro["autor"])
         print("Edad: " + str(libro["edad"]))
         print("Descripción: " + libro["descripcion"])
-        print("\n*****************************************************\n")
+        print("\n-------------------------------------------------------\n")
 
 def comprar_libro():
     None
 
 menu_usuarios()
+
+"/bin/python /run/media/mmcblk0p1/IdeaProjects/PROYECTO-PYTHON-SCRUM/menu-usuarios.py"
